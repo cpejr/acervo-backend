a6305ee7ae3df129d726dbdeb44f6be1642994c2
Alexandre Cenachi <alexandrecenachi@cpejr.com.br>
2023-03-30 19:19:33 -0300

    feat: express, cors, mongo and logger config

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+import corsOptions from './config/cors.js'
+import { NotFoundError } from './errors/baseErrors.js'
+// import routes from './routes/index.js'
+import isDevEnvironment from './utils/general/isDevEnviroment.js'
+
+// Inicializando instância do servidor express
+const app = express()
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(helmet())
+if (isDevEnvironment) app.use(morgan('dev'))
+
+// Routes
+// app.use('/api', routes)
+
+// Non existing routes
+app.all('*', (req, res, next) => {
+  next(new NotFoundError(`Route "${req.route}" not found`))
+})
+
+// Needs to be after the routes
+// app.use(deleteFilesOnError)
+// app.use(errorHandler)
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import startClusterServerInProd from './config/cluster'
+
+startClusterServerInProd()
